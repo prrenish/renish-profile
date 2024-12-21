4c6910bbffdbd05ff5110ad76db5019f63bea42f
Renish <prrenish@gmail.com>
2024-12-21 16:57:00 +0530

    initial commit

---
+++ src/content_option.js
@@ -0,0 +1,151 @@
+const logotext = "RENISH P R";
+const meta = {
+    title: "Renish P R",
+    description: "I’m Renish data scientist _ Full stack devloper,currently working in MResult",
+};
+
+const introdata = {
+    title: "I’m Renish",
+    animated: {
+        first: "I love coding",
+        second: "I make life easy",
+        third: "I am a solution expert",
+    },
+    description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed at nisl euismod urna bibendum",
+    your_img_url: "/assets/hacker-image.jpeg",
+};
+
+const dataabout = {
+    title: "about about my self",
+    aboutme: "Around 6+ years of experience in the field of development, carried on PHP, Javascript and Python based code and projects major in handling large scale complication handling projects.",
+};
+const worktimeline = [{
+        jobtitle: "Software Development Engineer",
+        where: "Xorstack",
+        date: "2018",
+    },
+    {
+        jobtitle: "Technical Analyist II",
+        where: "EMVIGO",
+        date: "2021",
+    },
+    {
+        jobtitle: "Senior Software Development Engineer",
+        where: "M-Result",
+        date: "2024",
+    },
+];
+
+const skills = [{
+        name: "Python",
+        value: 90,
+    },
+    {
+        name: "NodeJS",
+        value: 85,
+    },
+    {
+        name: "Javascript",
+        value: 80,
+    },
+    {
+        name: "ReactJS",
+        value: 90,
+    },
+    {
+        name: "AWS",
+        value: 85,
+    },
+];
+
+const services = [{
+        title: "UI & UX Design",
+        description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed at nisl euismod urna bibendum sollicitudin.",
+    },
+    {
+        title: "Mobile Apps",
+        description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed at nisl euismod urna bibendum sollicitudin.",
+    },
+    {
+        title: "Wordpress Design",
+        description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed at nisl euismod urna bibendum sollicitudin.",
+    },
+];
+
+const dataportfolio = [{
+        img: "https://picsum.photos/400/?grayscale",
+        description: "The National Detergent Company (NDC) oman along with the list application",
+        link: "https://www.ndcoman.com/",
+    },
+    {
+        img: "https://picsum.photos/400/800/?grayscale",
+        description: "Muhdo medical application handled the backend and full stack development",
+        link: "https://muhdo.com/",
+    },
+    {
+        img: "https://picsum.photos/400/?grayscale",
+        description: "We Swap, where you can buy sell and swap any car on the platform worked on the backend and ReactJS development",
+        link: "https://www.we-swap.com/",
+    },
+    {
+        img: "https://picsum.photos/400/600/?grayscale",
+        description: "Refinu, understand your own feelings and categories and improve your self or your organisation",
+        link: "https://www.refinu.com/",
+    },
+    {
+        img: "https://picsum.photos/400/300/?grayscale",
+        description: "Factstream, Extract the facts on the pdf using the AWS-Textract and highlight and improve the co ordinating extracts with PDFJS",
+        link: "https://factstream.ai/",
+    },
+    {
+        img: "https://picsum.photos/400/700/?grayscale",
+        description: "The wisdom of life consists in the elimination of non-essentials.",
+        link: "#",
+    },
+
+    {
+        img: "https://picsum.photos/400/600/?grayscale",
+        description: "The wisdom of life consists in the elimination of non-essentials.",
+        link: "#",
+    },
+    {
+        img: "https://picsum.photos/400/300/?grayscale",
+        description: "The wisdom of life consists in the elimination of non-essentials.",
+        link: "#",
+    },
+    {
+        img: "https://picsum.photos/400/?grayscale",
+        description: "The wisdom of life consists in the elimination of non-essentials.",
+        link: "#",
+    },
+];
+
+const contactConfig = {
+    YOUR_EMAIL: "prrenish@gmail.com",
+    YOUR_FONE: "+91 7676603777",
+    description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi vehicula eu nunc et sollicitudin. Cras pulvinar, nisi at imperdiet pharetra. ",
+    // creat an emailjs.com account 
+    // check out this tutorial https://www.emailjs.com/docs/examples/reactjs/
+    YOUR_SERVICE_ID: "service_id",
+    YOUR_TEMPLATE_ID: "template_id",
+    YOUR_USER_ID: "user_id",
+};
+
+const socialprofils = {
+    github: "https://github.com/prrenish",
+    facebook: "https://www.facebook.com/renish.raghunath",
+    linkedin: "https://www.linkedin.com/in/renish-raghunathan-b23b239a/",
+    instagram: "https://www.instagram.com/thelonelywo1f/",
+};
+export {
+    meta,
+    dataabout,
+    dataportfolio,
+    worktimeline,
+    skills,
+    services,
+    introdata,
+    contactConfig,
+    socialprofils,
+    logotext,
+};
